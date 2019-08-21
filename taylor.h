9f221a46a080b9e59bf1825b8e50e997d3b0a7ab
matheus.leal <matheus.leal@corp.globo.com>
2019-08-21 16:36:21 -0300

    módulo com funções para série de taylor

---
+++ taylor.h
@@ -0,0 +1,12 @@
+
+// função que aproxima a função f (x) = cos 2 x usando o polinômio de Taylor de grau 2 com centro em x0 = 0
+double cos2 (double x);
+
+// função que retorna o valor absoluto do resı́duo máximo da avaliação do polinômio para cos2() em x
+double cos2_rmax (double x);
+
+//função que aproxima a função f (x) = sqrt(x) usando o polinômio de Taylor de grau 3 com centro em x0 = 1.
+double raiz (double x);
+
+// função que retorna o valor absoluto do resı́duo máximo da avaliação do polinômio para raiz() em x
+double raiz_rmax (double x);
